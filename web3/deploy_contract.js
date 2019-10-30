14d29db94f03218ae3589c3d1d6f17267a58945f
austinabell <austinabell8@gmail.com>
2019-10-29 22:54:35 -0400

    MVP for contract deployment and interactions

--- web3/deploy_contract.js
+++ web3/deploy_contract.js
@@ -1,9 +1,6 @@
 const Web3 = require("web3");
 const fs = require("fs");
 
-// const web3 = new Web3(
-//   new Web3.providers.HttpProvider("http://127.0.0.1:7545")
-// );
 const web3 = new Web3(
   new Web3.providers.HttpProvider("http://localhost:8545/rpc")
 );
@@ -17,7 +14,7 @@ const testContract = new web3.eth.Contract(ABI);
 
 async function getCurrentAccount() {
   const currentAccounts = await web3.eth.getAccounts();
-  console.log("Unlocked account address: " + currentAccounts[0]);
+  console.log("Unlocked account address: \t", currentAccounts[0]);
   return currentAccounts[0];
 }
 async function deployContract(account) {
@@ -32,7 +29,7 @@ async function deployContract(account) {
       gasPrice: 1
     })
     .then(function(contractInstance) {
-      console.log("Contract Address: ", contractInstance.options.address);
+      console.log("Deployed contract Address: \t", contractInstance.options.address);
       return contractInstance;
     })
     .catch(function(err) {
@@ -47,11 +44,11 @@ async function contractAdd(contractInstance, sender) {
     .add()
     .send({
       from: sender,
-      gas: 400000,
+      gas: 30000,
       gasPrice: 1
     })
     .then(function(res) {
-      console.log("Add transaction completed at block: ", res.blockNumber);
+      console.log("Add transaction finalized in block: \t", res.blockNumber);
       return res;
     });
 }
@@ -66,11 +63,11 @@ async function getContractCounter(contractInstance) {
 async function run() {
   const sender = await getCurrentAccount();
   const contract = await deployContract(sender);
-  // const counter = await getContractCounter(contract);
-  // console.log("counter pre increment is: ", counter);
+  let counter = await getContractCounter(contract);
+  console.log("counter pre increment is: \t", counter);
   await contractAdd(contract, sender);
   counter = await getContractCounter(contract);
-  console.log("counter post increment is: ", counter);
+  console.log("counter post increment is: \t", counter);
 }
 
 run().then(() => console.log("done"));
